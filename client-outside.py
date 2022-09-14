6bcb3d2a2b21ba9466fb4b16ada0ffa083919841
Andrey Pokoev <nc101ux@gmail.com>
2022-09-14 15:48:24 +0300

    Move some scriptes to tools directory

--- client-outside.py
+++ client-outside.py
@@ -3,17 +3,12 @@
 import cv2
 from pprint import pprint
 
-#print("Before URL")
 cap = cv2.VideoCapture('rtsp://10.0.250.10:554/user=admin&password=&channel=2&stream=1.sdp?real_stream')
-#print("After URL")
 
 while True:
 
-    #print('About to start the Read command')
     ret, frame = cap.read()
-    #print('About to show frame of Video.')
     cv2.imshow("Outside",frame)
-    #print('Running..')
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
